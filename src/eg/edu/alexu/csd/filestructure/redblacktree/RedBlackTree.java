24455b5d8ade5b78022e4a1646d9a81d73543de0
Solovic2 <islammohamed323@gmail.com>
2020-04-10 11:59:57 +0200

    Add files via upload

--- src/eg/edu/alexu/csd/filestructure/redblacktree/RedBlackTree.java
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/RedBlackTree.java
@@ -1,5 +1,7 @@
 package eg.edu.alexu.csd.filestructure.redblacktree;
 
+import java.util.ArrayList;
+
 public class RedBlackTree <T extends Comparable<T>, V> implements IRedBlackTree<T,V> {
 	
 	private myNode root=null;
@@ -56,7 +58,11 @@ public class RedBlackTree <T extends Comparable<T>, V> implements IRedBlackTree<
 			}
 			
 			myNode k=insertion(root, key, value);
-			System.out.println(k.getKey());
+			System.out.println(" root "+k.getKey());
+			for(int i=0;i<arr.size();i++) {
+				System.out.println(" "+arr.get(i).getKey());
+			}
+			
 		}
 		
 	}
@@ -77,23 +83,69 @@ public class RedBlackTree <T extends Comparable<T>, V> implements IRedBlackTree<
 		}
 			return searching((myNode) node.getLeftChild() , key);
 	}
+	int l=0;
+	int r=0;
+	ArrayList <myNode> arr=new ArrayList <myNode>(4);
+	int count=1;
+
 	public myNode insertion(myNode node,T key,V value) {
 		myNode temp;
 		if(node == null) {
 			node = new myNode(key,value);
+			temp=node;
+//			arr= new ArrayList<myNode>(4);
+			if(arr.size()==0) arr.add((myNode) node);
+			else arr.set(0,(myNode) node);
+			count=1;
+			System.out.println("her is the last node"+arr.get(0).getKey());
 			return node;
-//			System.out.println(node.getKey());
 		}
 		int k = key.compareTo((T) node.getKey());
 		if( k > 0 ) {
 			 node.setRightChild(insertion((myNode) node.getRightChild() , key,value));
-			 node.getRightChild().setParent(node);
-//			System.out.println(node.getKey());
+			 r++;
 		}else {
 		  node.setLeftChild(insertion((myNode) node.getLeftChild() , key,value));
-		  node.getLeftChild().setParent(node);
-//			System.out.println(" "+node.getKey());
+		  l++;
 		}
+		if(node != null) {
+//			System.out.println("parents"+node.getKey());
+			if(arr.size()<=2)arr.add((myNode) node);
+			else arr.set(count, (myNode) node);
+//			System.out.println(arr.get(count).getKey()+ " here ");
+			if(count==2 && arr.get(count-1)!=null && node.getLeftChild()!=null) {
+//				System.out.println(arr.get(count-1).getKey()+ " beffffor" + " af " + node.getLeftChild().getKey());
+				if(arr.get(count-1).getKey()!=node.getLeftChild().getKey()) {
+//				 System.out.println("left "+node.getLeftChild().getKey());
+					if(arr.size()==3)arr.add((myNode) node.getLeftChild());
+					else arr.set(3, (myNode) node.getLeftChild());
+//				System.out.println(arr.get(3).getKey() + " left");
+				}
+				
+
+			}
+			 if(count==2 &&arr.get(count-1)!=null && node.getRightChild()!=null) {
+//					System.out.println(arr.get(count-1).getKey()+ " beffffor"+ "Df" + node.getRightChild().getKey());
+
+				if(arr.get(count-1).getKey()!=node.getRightChild().getKey()) {
+//				System.out.println("right "+node.getRightChild().getKey());
+				if(arr.size()==3)arr.add((myNode) node.getRightChild());
+
+				else arr.set(3,(myNode) node.getRightChild());
+//				System.out.println(arr.get(3).getKey() + " rgit");
+
+				}
+			 }
+//			 count =1;
+			 count++;
+			 if(count == 3) {
+				 count=1;
+			 }
+			 
+		}
+//		System.out.println(count );
+//		System.out.println(arr.size());
+
 		return node;
 	}
 	void inorderRec(myNode iNode) { 
--- src/eg/edu/alexu/csd/filestructure/redblacktree/node.java
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/node.java
@@ -1,4 +1,27 @@
 package eg.edu.alexu.csd.filestructure.redblacktree;
 
 public class node  {
+	public static void main(String[] args)  {
+		RedBlackTree<Integer,Integer>mm=new RedBlackTree<Integer, Integer>();
+	     mm.insert(50, 50); 
+	    mm.insert(30, 30); 
+	    mm.insert(20, 20); 
+	    mm.insert(40, 40); 
+	    mm.insert(70, 70); 
+	    mm.insert(60, 60); 
+	    mm.insert(80, 80);
+//	    mm.insert(90, 90);
+//	    mm.insert(85, 85);
+//	    mm.insert(66, 66);
+//	    mm.insert(58, 58);
+//	    mm.insert(44, 44);
+//	    mm.insert(32, 32);
+//	    mm.insert(21, 21);
+//	    mm.insert(19, 19);
+//	    mm.insert(85, 85);
+//	    mm.inorderRec((myNode) mm.getRoot());
+		
+	}
+	
+	
 }
